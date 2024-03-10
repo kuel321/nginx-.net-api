b81dd161af594fe3a8727ffa57c6b23a7fa693a7
kuel321 <lukeshort321@gmail.com>
2024-03-10 03:48:59 -0400

    changes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,7 +16,7 @@ if (app.Environment.IsDevelopment())
     
 }
 
-builder.Services.AddCors();
+
 
 app.UseCors(options =>
 options.AllowAnyOrigin()
